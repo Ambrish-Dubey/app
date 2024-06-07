5c66e582946c00875127c077639985f7f3da5230
Ambrish <ambrishpradeep1999@gmail.com>
2024-06-07 09:28:50 +0530

    second  commit

--- root/settings.py
+++ root/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
